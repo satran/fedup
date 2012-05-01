4a09fee96af3fe860849914d2fba31e820ae67af
Satyajit Ranjeev <satyajit.ranjeev@gmail.com>
2012-05-01 22:27:42 +0530

    Missed the datetime import

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from root.models import Tag
 import re
+import datetime
 
 class TaskList(models.Model):
     name = models.CharField(max_length=50)
