98e0a6914e79d546fc8f0500a30606e28d4a177e
Satyajit Ranjeev <satyajit.ranjeev@gmail.com>
2012-05-19 19:01:30 +0530

    Added multiple tasks support.

--- fedup/email_settings.py
+++ fedup/email_settings.py
@@ -1,15 +1,15 @@
 # Email Settings. Make sure you fill these if you want email to be send.
 # These are the settings for email to be send from fedup.
-EMAIL_HOST = ''
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'fedup@ranjeev.in'
+EMAIL_HOST_PASSWORD = "a's;dlfk"
 EMAIL_USE_TLS = True
 
 # Email settings for incoming mails.
-IMAP_HOST = ''
+IMAP_HOST = 'imap.google.com'
 IMAP_USE_SSL = True
 IMAP_PORT = 993
-IMAP_USER_NAME = ''
-IMAP_USER_PASS = ""
+IMAP_USER_NAME = 'fedup@ranjeev.in'
+IMAP_USER_PASS = "a's;dlfk"
 
